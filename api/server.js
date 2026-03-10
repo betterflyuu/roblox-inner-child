ea6d3263d7b550b4d771639fb9e693564af46079
betterflyuu <iksanmlv@gmail.com>
2026-03-11 05:55:16 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -12,8 +12,7 @@ app.post("/api/server", async (req, res) => {
     }
 
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-   const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
-
+   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
     const { pesan, gender, nama } = req.body;
 
     const systemPrompt = `Kamu adalah versi kecil dari ${nama}. Gender kamu ${gender}. 
